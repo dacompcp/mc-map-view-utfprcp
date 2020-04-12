d5c387de2b0b7c8b283ce233e26f5323dd3e8f6a
Guihgo <guihgo100milha@gmail.com>
2020-04-12 04:09:56 -0300

    commit initial

---
+++ markers.js
@@ -0,0 +1,18 @@
+var markers={
+  "utfprcp": [
+    {
+      "groupName": "Blocos7540_778d",
+      "displayName": "Blocos",
+      "icon": "signpost_icon.png",
+      "createInfoWindow": true,
+      "checked": false
+    },
+    {
+      "groupName": "Lugares_Importantes8ac9_778d",
+      "displayName": "Lugares Importantes",
+      "icon": "signpost_icon.png",
+      "createInfoWindow": true,
+      "checked": true
+    }
+  ]
+};
