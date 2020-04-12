b21dcae2907e4e9ee1615bd7a915849e465caa20
Guihgo <guihgo100milha@gmail.com>
2020-04-12 06:25:29 -0300

    add Bloco K

--- markers.js
+++ markers.js
@@ -1,21 +1,21 @@
 var markers={
   "utfprcp": [
     {
-      "groupName": "Blocos2ab8_b220",
+      "groupName": "Blocosaab8_1b83",
       "displayName": "Blocos",
       "icon": "signpost_icon.png",
       "createInfoWindow": true,
       "checked": false
     },
     {
-      "groupName": "Lugares_Importantes2ac9_b220",
+      "groupName": "Lugares_Importantesaac9_1b83",
       "displayName": "Lugares Importantes",
       "icon": "signpost_icon.png",
       "createInfoWindow": true,
       "checked": true
     },
     {
-      "groupName": "CA/DA/Proj.Pesq.Ext.d540_b220",
+      "groupName": "CA/DA/Proj.Pesq.Ext.5540_1b83",
       "displayName": "CA/DA/Proj.Pesq.Ext.",
       "icon": "signpost_icon.png",
       "createInfoWindow": true,
--- markersDB.js
+++ markersDB.js
@@ -1,5 +1,129 @@
 var markersDB={
-  "CA/DA/Proj.Pesq.Ext.d540_b220": {
+  "Lugares_Importantesaac9_1b83": {
+    "created": false,
+    "raw": [
+      {
+        "x": 182,
+        "y": 64,
+        "z": -125,
+        "icon": "images/24horas.png",
+        "text": "Espa\u00e7o de funcionamento 24horas/7dias da semana com computadores e internet",
+        "hovertext": "24 Horas"
+      },
+      {
+        "x": 178,
+        "y": 64,
+        "z": -138,
+        "icon": "images/hall.png",
+        "text": "Espa\u00e7o para trabalhos em grupo, conversar, descansar e chorar",
+        "hovertext": "Hall"
+      },
+      {
+        "x": 219,
+        "y": 64,
+        "z": -144,
+        "icon": "images/anfitetatro.png",
+        "text": "Vasto edif\u00edcio de forma circular destinado \u00e0 apresenta\u00e7\u00f5es, eventos, palestras, confer\u00eancias...",
+        "hovertext": "Anfiteatro"
+      },
+      {
+        "x": 214,
+        "y": 64,
+        "z": -111,
+        "icon": "images/ru.png",
+        "text": "Restaurante Universit\u00e1rio - (Seg. - Sext.) Almo\u00e7o das 11H30 \u00e0s 14H00 e janta 17H30 \u00c0s 21H30. E almo\u00e7o aos S\u00e1bados das 11H30 \u00e0s 14H00",
+        "hovertext": "R.U."
+      },
+      {
+        "x": 191,
+        "y": 64,
+        "z": -119,
+        "icon": "images/biblioteca.png",
+        "text": "Biblioteca",
+        "hovertext": "Biblioteca"
+      },
+      {
+        "x": 339,
+        "y": 64,
+        "z": 61,
+        "icon": "images/quadra.png",
+        "text": "Quadra esportiva",
+        "hovertext": "Quadra"
+      }
+    ],
+    "name": "Lugares Importantes"
+  },
+  "Blocosaab8_1b83": {
+    "created": false,
+    "raw": [
+      {
+        "x": 175,
+        "y": 64,
+        "z": -32,
+        "icon": "images/bloco_A.png",
+        "text": "Bloco A: Normalmente utilizado para as salas teoricas de f\u00edsica, qu\u00edmica, eletr\u00f4nica, el\u00e9trica, mec\u00e2nica e pr\u00e1ticas de qu\u00edmica",
+        "hovertext": "BLOCO A"
+      },
+      {
+        "x": 250,
+        "y": 64,
+        "z": -111,
+        "icon": "images/bloco_G.png",
+        "text": "Bloco G: Normalmente utilizado para as salas pr\u00e1ticas de eletr\u00f4nica. No \u00faltimo andar h\u00e1 salas de PAluno",
+        "hovertext": "BLOCO G"
+      },
+      {
+        "x": 238,
+        "y": 64,
+        "z": -93,
+        "icon": "images/bloco_H.png",
+        "text": "Bloco H: Une o Blobo G com I. Sugest\u00e3o?: envie para o Guihgo ( guioli.1998@alunos.utfpr.edu.br )",
+        "hovertext": "BLOCO H"
+      },
+      {
+        "x": 246,
+        "y": 64,
+        "z": -72,
+        "icon": "images/bloco_I.png",
+        "text": "Bloco I: Aqui tem o maravilhoso almoxarifado de componentes eletr\u00f4nicos da UTFPR, al\u00e9m de laborat\u00f3rios de el\u00e9trica/eletr\u00f4nica",
+        "hovertext": "BLOCO I"
+      },
+      {
+        "x": 218,
+        "y": 64,
+        "z": -22,
+        "icon": "images/bloco_K.png",
+        "text": "Bloco K: Salas de PAs dos professores da computa\u00e7\u00e3o e uma sala no fundo cheia de MACs",
+        "hovertext": "BLOCO K"
+      },
+      {
+        "x": 230,
+        "y": 64,
+        "z": 57,
+        "icon": "images/bloco_P.png",
+        "text": "Bloco P: Nesse bloco acontecem a maioria das aulas da computa\u00e7\u00e3o, possui uma vista previlegiada para o campo, al\u00e9m de laborat\u00f3rios",
+        "hovertext": "BLOCO P"
+      },
+      {
+        "x": 229,
+        "y": 64,
+        "z": -185,
+        "icon": "images/bloco_R.png",
+        "text": "Bloco R: Ainda esperando ficar pronto :/",
+        "hovertext": "BLOCO R"
+      },
+      {
+        "x": 279,
+        "y": 64,
+        "z": -160,
+        "icon": "images/bloco_S.png",
+        "text": "Bloco S: Ainda esperando ficar pronto, apesar de j\u00e1 ter algumas aulas l\u00e1",
+        "hovertext": "BLOCO S"
+      }
+    ],
+    "name": "Blocos"
+  },
+  "CA/DA/Proj.Pesq.Ext.5540_1b83": {
     "created": false,
     "raw": [
       {
@@ -68,121 +192,5 @@ var markersDB={
       }
     ],
     "name": "CA/DA/Proj.Pesq.Ext."
-  },
-  "Lugares_Importantes2ac9_b220": {
-    "created": false,
-    "raw": [
-      {
-        "x": 182,
-        "y": 64,
-        "z": -125,
-        "icon": "images/24horas.png",
-        "text": "Espa\u00e7o de funcionamento 24horas/7dias da semana com computadores e internet",
-        "hovertext": "24 Horas"
-      },
-      {
-        "x": 178,
-        "y": 64,
-        "z": -138,
-        "icon": "images/hall.png",
-        "text": "Espa\u00e7o para trabalhos em grupo, conversar, descansar e chorar",
-        "hovertext": "Hall"
-      },
-      {
-        "x": 219,
-        "y": 64,
-        "z": -144,
-        "icon": "images/anfitetatro.png",
-        "text": "Vasto edif\u00edcio de forma circular destinado \u00e0 apresenta\u00e7\u00f5es, eventos, palestras, confer\u00eancias...",
-        "hovertext": "Anfiteatro"
-      },
-      {
-        "x": 214,
-        "y": 64,
-        "z": -111,
-        "icon": "images/ru.png",
-        "text": "Restaurante Universit\u00e1rio - (Seg. - Sext.) Almo\u00e7o das 11H30 \u00e0s 14H00 e janta 17H30 \u00c0s 21H30. E almo\u00e7o aos S\u00e1bados das 11H30 \u00e0s 14H00",
-        "hovertext": "R.U."
-      },
-      {
-        "x": 191,
-        "y": 64,
-        "z": -119,
-        "icon": "images/biblioteca.png",
-        "text": "Biblioteca",
-        "hovertext": "Biblioteca"
-      },
-      {
-        "x": 339,
-        "y": 64,
-        "z": 61,
-        "icon": "images/quadra.png",
-        "text": "Quadra esportiva",
-        "hovertext": "Quadra"
-      }
-    ],
-    "name": "Lugares Importantes"
-  },
-  "Blocos2ab8_b220": {
-    "created": false,
-    "raw": [
-      {
-        "x": 175,
-        "y": 64,
-        "z": -32,
-        "icon": "images/bloco_A.png",
-        "text": "Bloco A: Normalmente utilizado para as salas teoricas de f\u00edsica, qu\u00edmica, eletr\u00f4nica, el\u00e9trica, mec\u00e2nica e pr\u00e1ticas de qu\u00edmica",
-        "hovertext": "BLOCO A"
-      },
-      {
-        "x": 250,
-        "y": 64,
-        "z": -111,
-        "icon": "images/bloco_G.png",
-        "text": "Bloco G: Normalmente utilizado para as salas pr\u00e1ticas de eletr\u00f4nica. No \u00faltimo andar h\u00e1 salas de PAluno",
-        "hovertext": "BLOCO G"
-      },
-      {
-        "x": 238,
-        "y": 64,
-        "z": -93,
-        "icon": "images/bloco_H.png",
-        "text": "Bloco H: Une o Blobo G com I. Sugest\u00e3o?: envie para o Guihgo ( guioli.1998@alunos.utfpr.edu.br )",
-        "hovertext": "BLOCO H"
-      },
-      {
-        "x": 246,
-        "y": 64,
-        "z": -72,
-        "icon": "images/bloco_I.png",
-        "text": "Bloco I: Aqui tem o maravilhoso almoxarifado de componentes eletr\u00f4nicos da UTFPR, al\u00e9m de laborat\u00f3rios de el\u00e9trica/eletr\u00f4nica",
-        "hovertext": "BLOCO I"
-      },
-      {
-        "x": 230,
-        "y": 64,
-        "z": 57,
-        "icon": "images/bloco_P.png",
-        "text": "Bloco P: Nesse bloco acontecem a maioria das aulas da computa\u00e7\u00e3o, possui uma vista previlegiada para o campo, al\u00e9m de laborat\u00f3rios",
-        "hovertext": "BLOCO P"
-      },
-      {
-        "x": 229,
-        "y": 64,
-        "z": -185,
-        "icon": "images/bloco_R.png",
-        "text": "Bloco R: Ainda esperando ficar pronto :/",
-        "hovertext": "BLOCO R"
-      },
-      {
-        "x": 279,
-        "y": 64,
-        "z": -160,
-        "icon": "images/bloco_S.png",
-        "text": "Bloco S: Ainda esperando ficar pronto, apesar de j\u00e1 ter algumas aulas l\u00e1",
-        "hovertext": "BLOCO S"
-      }
-    ],
-    "name": "Blocos"
   }
 };
